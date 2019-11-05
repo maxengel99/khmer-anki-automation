30b54c4fe49e7919a9646590786d85b2bbcbe46d
Max Engel <maxengel99@gmail.com>
2019-11-05 09:53:26 +0700

    rewrite letter combination as class

--- khmer-letter-combination.py
+++ khmer-letter-combination.py
@@ -1,16 +1,23 @@
 import requests
 
-consonant_arr = ['ក','គ','ខ','ឃ',]
-vowel_arr = ['','ា', 'ិ']
+class KhmerLetters:
 
-combined_arr = []
-for consonant in consonant_arr:
-    for vowel in vowel_arr:
-        combined_arr.append(consonant + vowel)
+    def __init__(self):
+        self.consonant_arr = ['ក','គ','ខ','ឃ']
+        self.vowel_arr = ['','ា', 'ិ']
+    
+    def create_combinations(self):
+        combined_arr = []
+        for consonant in self.consonant_arr:
+            for vowel in self.vowel_arr:
+                combined_arr.append(consonant + vowel)
+        
+        return combined_arr
 
-for combination in combined_arr:
-    url="https://translate.google.com/translate_tts?ie=UTF-8&tl=km&client=tw-ob&q=" + combination
-    doc = requests.get(url)
-    filename = 'files/' + combination + ".mp3"
-    with open(filename, "wb") as f:
-        f.write(doc.content)
+    def get_audio(self, combined_arr):
+        for combination in combined_arr:
+            url="https://translate.google.com/translate_tts?ie=UTF-8&tl=km&client=tw-ob&q=" + combination
+            doc = requests.get(url)
+            filename = 'files/' + combination + ".mp3"
+            with open(filename, "wb") as f:
+                f.write(doc.content)
