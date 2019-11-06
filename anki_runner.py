823d03eea19e17c5dc10a708cb0ec9a673f52cf0
Max Engel <maxengel99@gmail.com>
2019-11-06 14:42:34 +0700

    move execution to main function

---
+++ anki_runner.py
@@ -0,0 +1,15 @@
+from KhmerLetters import KhmerLetters
+from AnkiRequest import AnkiRequest
+
+def main():
+    khmer_letters = KhmerLetters()
+    anki_request = AnkiRequest()
+    combination_arr = khmer_letters.create_combinations()
+
+    for combination in combination_arr:
+        anki_arg = anki_request.generate_json_args(combination)
+        response = anki_request.invoke(anki_arg)
+        print(response)
+
+if __name__ == '__main__':
+    main()
