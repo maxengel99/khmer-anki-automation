f9124d8a793c77bc8fb7c798b397195fa7419f72
Max Engel <maxengel99@gmail.com>
2020-02-26 17:15:54 +0700

    test audio

--- begin_dialogue.py
+++ begin_dialogue.py
@@ -1,15 +1,17 @@
 import easygui
+import os
+from github_handler import GithubHandler
 from helper import create_audio
 
 
 def begin():
-        
+    '''Starts the textbox conversation'''
+
     category = easygui.buttonbox("Would you like to upload letters or vocabulary?", choices=('Letters', 'Vocabulary'))
-    
-    if(category.lower() == "vocabulary"):
+
+    if(category == "Vocabulary"):
         vocab_file_name = easygui.fileopenbox("Please upload a textfile of the new vocabulary.")
         vocab_file_open = (open(vocab_file_name, 'r', encoding='utf8', errors='ignore'))
-
         vocab_file_content = vocab_file_open.readlines()
 
         khmer_english_pair_arr = []
@@ -17,12 +19,23 @@ def begin():
             khmer_english_pair = line.rstrip().split('/')
             khmer_english_pair_arr.append(khmer_english_pair)
         
-        for pair in khmer_english_pair:
-            create_audio("words", pair[0])
+        audio_option = easygui.ynbox("Do you need to create audio?", choices=("Yes", "No"))
 
-    
+        if audio_option:
+            github_handler = GithubHandler()
+
+            for pair in khmer_english_pair_arr:
+                if not os.path.isfile('files/words/{}.mp3'.format(pair[0])):
+                    create_audio("words", pair[0])
+                    print("Creating audio for - {}".format(pair[1]))
+                else:
+                    print("Skipping word - {}".format(pair[1]))
+            
+            commit_message = easygui.enterbox()
+            github_handler.add_to_github(commit_message)
+             
     elif(category.lower() == "letters"):
-        easygui.msgbox("etters")
+        easygui.msgbox("letters")
 
     user_continue = easygui.ynbox("Would you like to perform another command?", choices=("Yes", "No"))
     if user_continue:
