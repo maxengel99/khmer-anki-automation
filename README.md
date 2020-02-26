ef5888a5f01c78359ae3f77ee28c3dd31dbb7a26
Max Engel <maxengel99@gmail.com>
2020-02-26 19:23:12 +0700

    Attempt to use Soundoftext API - timeout error

---
+++ README.md
@@ -0,0 +1,3 @@
+# Khmer Anki Automation
+
+Allows me to create Khmer audio files and add them to an Anki deck. Makes creating flashcards much easier.
--- helper.py
+++ helper.py
@@ -1,5 +1,6 @@
 '''Deals with audio'''
 import requests
+import json
 
 
 def create_audio(category, word):
@@ -15,3 +16,25 @@ def create_audio(category, word):
     with open(filename, "wb") as file:
         print('writing file {}'.format(filename))
         file.write(doc.content)
+
+
+def create_audio_two(text):
+    '''Creates audio file using Soundoftext API'''
+
+    url = "https://api.soundoftext.com"
+    headers = {'Content-type': 'application/json'}
+    post_body = {
+        "engine": "Google",
+        "data": {
+            "text": text,
+            "voice": "km"
+        }
+    }
+
+    response = requests.post(url, json=post_body, timeout=3)
+
+    print(response.text)
+
+
+if __name__ == '__main__':
+    create_audio_two("សួស្តី")
