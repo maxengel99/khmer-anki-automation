09deba4ae1808c825596171f46983d911876ca9e
Max Engel <maxengel99@gmail.com>
2020-02-26 18:58:11 +0700

    Delete old py and txt files

--- github_handler.py
+++ github_handler.py
@@ -30,4 +30,3 @@ class GithubHandler:
 
         origin = repo.remote(name="origin")
         origin.push()
-        
--- test.py
+++
@@ -1,6 +0,0 @@
-letters = ['ក', 'ប']
-vowels = ['ៀ', 'េ']
-
-for letter in letters:
-    for vowel in vowels:
-        print(letter + vowel)
